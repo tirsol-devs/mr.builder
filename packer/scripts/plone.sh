e2424e4a80765767521c0a32503a145fb310451d
sven <sven@so36.net>
2015-05-07 15:19:22 +0200

    WIP package listx

--- packer/scripts/plone.sh
+++ packer/scripts/plone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Install dependecies for Plone [https://plone.org]
-apt-get install htop build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-setuptools python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils python-psutil python-support git vim libz-dev
+apt-get install htop python2.7 build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils python-psutil python-support git vim libz-dev
 
 # Download Installer
 wget --no-check-certificate https://launchpad.net/plone/4.3/4.3.4/+download/Plone-4.3.4-UnifiedInstaller.tgz
