9ca99e3cbaf332a9055cdbe1cdf5aefc72a10fe9
sven <sven@so36.net>
2015-05-12 10:34:16 +0200

    Fix permissions

--- packer/scripts/plone.sh
+++ packer/scripts/plone.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -eux
 
 # Install dependecies for Plone [https://plone.org]
-apt-get install htop python2.7 build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils python-psutil python-support git vim libz-dev
+apt-get -y install htop python2.7 build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils python-psutil python-support git vim libz-dev
 
 # Download Installer
-wget --no-check-certificate https://launchpad.net/plone/4.3/4.3.4/+download/Plone-4.3.4-UnifiedInstaller.tgz
+su -c "wget --no-check-certificate https://launchpad.net/plone/4.3/4.3.4/+download/Plone-4.3.4-UnifiedInstaller.tgz" vagrant
 
 # Unpack Installer
-tar -xf Plone-4.3.4-UnifiedInstaller.tgz
+su -c "tar -xf Plone-4.3.4-UnifiedInstaller.tgz" vagrant
 
 # Run Installer
-./Plone-4.3.4-UnifiedInstaller/install.sh --password=plone standalone
+su -c "./Plone-4.3.4-UnifiedInstaller/install.sh --password=plone standalone" vagrant
 
 # Delete old files
 rm -rf Plone-4.3.4-UnifiedInstaller.tgz
